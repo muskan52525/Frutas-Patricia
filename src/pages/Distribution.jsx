f1c5f00d61e4201fd4d2c49154bf95f395e72141
muskan52525 <135335716+muskan52525@users.noreply.github.com>
2025-10-03 13:27:12 +0530

    Update Distribution.jsx

--- src/pages/Distribution.jsx
+++ src/pages/Distribution.jsx
@@ -57,11 +57,11 @@ export default function Distribution() {
                         rounded-4xl md:rounded-full shadow-xl ring-1 ring-black/5
                         px-6 sm:px-10 py-8"
           >
-            <ul className="grid grid-cols-1 sm:grid-cols-3 gap-y-6 sm:gap-y-0 sm:gap-x-12 text-center max-w-2xl">
+            <ul className="grid grid-cols-1 sm:grid-cols-3 gap-y-6 sm:gap-y-0 sm:gap-x-12 text-center max-w-3xl items-center pr-0 lg:pr-3">
               {stats.map((s, i) => (
                 <li
                   key={i}
-                  className="flex flex-col items-center justify-center"
+                  className="flex flex-col items-center justify-center pr-7"
                 >
                   <span className="text-3xl sm:text-4xl lg:text-[60px] font-bold leading-none">
                     {s.value}
