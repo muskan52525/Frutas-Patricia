fd6d77d837d1ce862e0cbfc73d96b500b237682e
muskan52525 <135335716+muskan52525@users.noreply.github.com>
2025-10-03 13:06:36 +0530

    Update Production.jsx

--- src/pages/Production.jsx
+++ src/pages/Production.jsx
@@ -19,7 +19,7 @@ export default function Productions() {
           <div className="mx-auto max-w-full pl-0 sm:pl-6 lg:pl-8">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-20 items-center lg:gap-50">
               {/* Left: Text content */}
-              <div className="text-center lg:text-left justify-end sm:justify-center md:justify-center lg:justify-center grid px-5 ml-0 md:ml-0  lg:ml-[20px]">
+              <div className="text-center lg:text-left justify-end sm:justify-center md:justify-center lg:justify-center grid px-5 ml-0 md:ml-0  lg:ml-[180px]">
                 <h2 className="text-3xl sm:text-4xl lg:text-5xl font-extrabold leading-tight tracking-tight">
                   <span className="text-[var(--color-prime)]">
                     O melhor da produção nacional
